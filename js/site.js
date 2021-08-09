77ca47c18f60017ff4f9eb07e570422f7b2cb9c0
Walter Cyrulik <wcyrulik@cox.net>
2021-08-09 19:32:00 -0400

    Commit completed Rewind Exercise
    Completed the Rewind javascript exercise.

--- js/site.js
+++ js/site.js
@@ -1,4 +1,24 @@
-//Call Hello World
-function helloWorld() {
-    alert("Hello World");
+// Get the string from the page
+function getValue() {
+
+    document.getElementById("alert").classList.add("invisible");
+    let userString = document.getElementById("userString").value;
+    let revString = reverseString(userString);
+    displayString(revString);
 }
+
+//Reverse the string
+function reverseString(userString){
+    let revString = "";
+    for (let index = userString.length - 1; index >= 0; index--) {
+        revString += userString[index]; 
+    }
+    return revString;
+}
+
+//Display the reversed string back on the page.
+function displayString(revstring){
+    document.getElementById("msg").innerHTML = `Your string reversed is: ${revstring}`;
+    document.getElementById("alert").classList.remove("invisible");
+}
+
